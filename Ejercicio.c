5190a3ac23c0c6655175644246d130844af2ced9
monseeker <guesam460@gmail.com>
2025-04-26 22:31:11 -0300

    Ejercicio con errores

--- Ejercicio.c
+++ Ejercicio.c
@@ -9,7 +9,7 @@ typedef struct {
     int Duracion; // entre 10 – 100
 } Tarea;
 
-typedef struct {
+typedef struct Nodo {
     Tarea T;
     struct Nodo *Siguiente;
 } Nodo;
@@ -17,40 +17,40 @@ typedef struct {
 Nodo * crearListaVacia();
 Nodo * crearNodo(char descripcion[], int duracion, int id);
 void insertarNodo(Nodo **start, Nodo *nodo);
+int cargarTareas(int id, Nodo ** start);
 void mostrarNodos(Nodo ** start);
+Nodo * buscarNodo(Nodo ** start, int id);
+Nodo * quitarNodo(Nodo ** start, int id);
+void moverNodo(Nodo **startRealizados, Nodo **startPendientes, int id);
 
 int main(){
-    int aux=1, id=1000;
-    Nodo * start = crearListaVacia();
+    int id=1000, aux, idTarea;
+    Nodo * startPendientes = crearListaVacia();
+    Nodo * startRealizadas = crearListaVacia();
     
     do
     {
-        int duracionTarea;
-        char buffer[MAX];
-
-        printf("Ingrese una descripcion de la tarea: ");
-        fgets(buffer, MAX, stdin);
-        buffer[strcspn(buffer, "\n")] = '\0';
-        
-        printf("Ingrese la duracion de la tarea (entte 10 y 100): ");
-        scanf("%d", &duracionTarea);
-        getchar();
-        if (duracionTarea >= 10 && duracionTarea <= 100){
-            Nodo * tareaPendiente = crearNodo(buffer, duracionTarea, id);
-            insertarNodo(&start, tareaPendiente);
-            printf("¿Desea ingresar una nueva tarrea? \n0. No\n1. Si\n");
-            scanf("%d", &aux);
-            getchar();
-        } else {
-            printf("Ingrese un numero entre 10 y 100.\n");
-            return 0;
-        }
-        
+        aux=cargarTareas(id, &startPendientes);
         id++;
     } while (aux != 0);
 
-    mostrarNodos(&start);
+    printf("\n");
+    printf("Tareas Pendientes: \n");
+    mostrarNodos(&startPendientes);
+
+    printf("Ingrese el ID de la tarea realizada: ");
+    scanf("%d", &idTarea);
+    getchar();
+    moverNodo(&startRealizadas, &startPendientes, idTarea);
+   
     
+    printf("\n");
+    printf("Tareas Pendientes: \n");
+    mostrarNodos(&startPendientes);
+    
+    printf("\n");
+    printf("Tareas Realizadas: \n");
+    mostrarNodos(&startRealizadas);
     
     return 0;
 }
@@ -73,14 +73,39 @@ Nodo * crearNodo(char descripcion[], int duracion, int id){
     return tareaPendiente;
 }
 
-void insertarNodo(Nodo **start, Nodo * nodo){
+void insertarNodo(Nodo ** start, Nodo *nodo){
     nodo->Siguiente = *start;
     *start = nodo;
 }
 
+int cargarTareas(int id, Nodo ** start){
+    int duracionTarea;
+        int bool;
+        char buffer[MAX];
+
+        printf("Ingrese una descripcion de la tarea: ");
+        fgets(buffer, MAX, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        
+        printf("Ingrese la duracion de la tarea (entre 10 y 100): ");
+        scanf("%d", &duracionTarea);
+        getchar();
+        if (duracionTarea >= 10 && duracionTarea <= 100){
+            Nodo * tareaPendiente = crearNodo(buffer, duracionTarea, id);
+            insertarNodo(start, tareaPendiente);
+            printf("Desea ingresar una nueva tarea? \n0. No\n1. Si\n");
+            scanf("%d", &bool);
+            getchar();
+            return bool;
+        } else {
+            printf("Ingrese un numero entre 10 y 100.\n");
+            return 0;
+        }
+        
+}
+
 void mostrarNodos(Nodo ** start){
     Nodo * AuxNodo = *start;
-    printf("\nNodos\n");
     while (AuxNodo)
     {
         printf("Id: %d \n", AuxNodo->T.TareaID);
@@ -89,4 +114,72 @@ void mostrarNodos(Nodo ** start){
         printf("\n");
         AuxNodo = AuxNodo->Siguiente;
     }
+}
+
+Nodo * buscarNodo(Nodo ** start, int id){
+    Nodo * auxNodo = *start;
+    while (auxNodo && auxNodo->T.TareaID != id)
+    {
+        auxNodo = auxNodo->Siguiente;
+    }
+    return auxNodo;    
+}
+
+Nodo * quitarNodo(Nodo ** start, int id){
+    Nodo ** auxNodo = start;
+
+    while (*auxNodo && (*auxNodo)->T.TareaID != id)
+    {
+        auxNodo = &(*auxNodo)->Siguiente;
+    }
+
+
+
+    if(*auxNodo){
+        Nodo *temp = *auxNodo;
+        *auxNodo = (*auxNodo)->Siguiente;
+        temp->Siguiente = NULL;
+        return temp;
+    }
+    return NULL;
+    
+}
+
+void moverNodo(Nodo **startRealizados, Nodo **startPendientes, int id){
+    int bool;
+    Nodo * auxNodo = *startRealizados;
+    Nodo * temp = buscarNodo(startPendientes, id);
+
+    if (temp == NULL) {
+        printf("Error: No se encontró el nodo con ID %d.\n", id);
+        return;
+    }
+
+    if (auxNodo == NULL) {
+        *startRealizados = temp;
+        temp->Siguiente = NULL;
+        return;
+    }
+
+    while ((auxNodo)->Siguiente != NULL)
+    {
+        auxNodo = auxNodo->Siguiente;
+    }
+    printf("\n");
+    printf("Nodo a mover: \n");
+    printf("    ID: %d \n", temp->T.TareaID);
+    printf("    Descripcion: %s \n", temp->T.Descripcion);
+    printf("    Duracion: %d \n", temp->T.Duracion);
+    printf("\n");
+    printf("Confirma la operacion?  0.No  1.Si \n");
+    scanf("%d", &bool);
+    getchar();
+
+    if(bool){
+        auxNodo->Siguiente = temp;
+        temp->Siguiente = NULL;
+        Nodo * eliminar = quitarNodo(startPendientes, id);
+        free(eliminar);
+    } 
+
 }
